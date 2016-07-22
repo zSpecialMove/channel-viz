b9294eb2d2465b37e76076197688a83ca680dbb8
zSpecialMove <schiefer.ali@gmx.at>
2016-07-22 15:58:13 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,8 +17,8 @@
 		applicationName	= 'Visualisierung FBH-Anhänger', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= '58FA58', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
+		dataColor	= '58FA58', // CSS HEX value of color to represent data (omit leading #)
+		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
