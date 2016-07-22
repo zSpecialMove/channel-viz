eb8a51c99bc72ed2f487fbaeaaca4f0c27a27815
zSpecialMove <schiefer.ali@gmx.at>
2016-07-22 15:51:16 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,15 +1,15 @@
 (function ( $ ){
 
 	/*
-	EXAMPLE CONFIGURATION
+	zSpecialMove's CONFIGURATION
 
-		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
+		var defaultKey	= 'qaD0dFAoHM7uWgaU0CDkFi1npsdyu9B8LG83WFNZ19XHcbAQ', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1279051540], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Visualisierung FBH-Anhänger', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0;
+		dataColor		= '58FA58', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1;
 	*/
 
 	var defaultKey		= '', // Unique master Xively API key to be used as a default
